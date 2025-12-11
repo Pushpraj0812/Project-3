360187e8284f4e71c67c9b7775bd269a6cbe89b4
Pushpraj0812 <167536056+Pushpraj0812@users.noreply.github.com>
2025-12-12 00:53:05 +0530

    12-12-2025

---
+++ Project-3/src/main/java/in/co/rays/project_3/controller/FrontController.java
@@ -0,0 +1,57 @@
+package in.co.rays.project_3.controller;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
+import in.co.rays.project_3.util.ServletUtility;
+
+/**
+ * Front Functionality ctl. to perform session checking and logging operation
+ * @author Pushpraj Singh Kachhaway
+ *
+ */
+@WebFilter(urlPatterns={"/ctl/*","/doc/*"})
+public class FrontController implements Filter {
+	
+	private static Logger log = Logger.getLogger(FrontController.class);
+	
+	public void init(FilterConfig conf) throws ServletException {
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		
+		log.debug("FrontController dofilter start");
+		
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
+		
+		HttpSession session = request.getSession();
+		
+		String uri=request.getRequestURI();
+		request.setAttribute("uri", uri);
+		
+		if (session.getAttribute("user") == null) {
+			request.setAttribute("error", "Your session has been expired please Login again!");
+			ServletUtility.forward(ORSView.LOGIN_VIEW, request, response);
+		} else {
+			chain.doFilter(req, resp);
+		}
+		log.debug("FrontController dofilter end");
+	}
+
+	public void destroy() {
+	}
+}
